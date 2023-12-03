246f391b54b306bc4d6f3d9896628d7a75391147
Shreyas A S <137637016+shreyas-a-s@users.noreply.github.com>
2023-12-03 14:17:02 +0530

    Install gnome-boxes if gnome is the de

--- install.sh
+++ install.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-sudo pacman -S qemu-base virt-manager virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat ebtables iptables libguestfs --noconfirm
+sudo pacman -S qemu-base virt-viewer dnsmasq vde2 bridge-utils openbsd-netcat ebtables iptables libguestfs --noconfirm
 
+if [ "$XDG_CURRENT_DESKTOP" = "GNOME" ]; then
+  sudo pacman -S gnome-boxes --noconfirm
+else
+  sudo pacman -S virt-manager --noconfirm
+fi
+  
 sudo usermod -a -G libvirt $(whoami)
 newgrp libvirt
 
